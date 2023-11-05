a00282c562d51c7ef69b7f4d1d7ca9edcfb36779
Squeak-Paradox <olliemchurch@gmail.com>
2023-11-05 12:59:52 +0000

    Fixes incorrect nav button

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -9,8 +9,8 @@
                 <b-navbar-nav class="ms-auto pt-4 pt-lg-0">
                     <router-link v-if="!user" to="/login" class="px-3 pb-0 mb-0 h4 text-decoration-none">Login</router-link>
                     <template v-else>
-                        <router-link to="/" class="pe-lg-4 pb-0 mb-0 h4 text-decoration-none">Dashboard</router-link>
-                        <router-link to="/tasks" class="pe-lg-4 pb-0 mb-0 h4 text-decoration-none">Tasks</router-link>
+                        <router-link to="/dashboard" class="pe-lg-4 pb-0 mb-0 h4 text-decoration-none">Dashboard</router-link>
+                        <router-link to="/" class="pe-lg-4 pb-0 mb-0 h4 text-decoration-none">Tasks</router-link>
                         <router-link to="/schedule" class="pe-lg-4 pb-0 mb-0 h4 text-decoration-none">Glitch</router-link>
                         <router-link to="/settings" class="pb-0 mb-0 h4 text-decoration-none">Settings</router-link>
                     </template>
